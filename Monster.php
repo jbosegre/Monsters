1fdd4bb551c7df4ec89fa7d64f5c4ff435373faa
jbosegre <jbose@mail.greenriver.edu>
2018-02-08 16:21:34 -0500

    Added the Monster class

---
+++ Monster.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: humme_000
+ * Date: 2/8/2018
+ * Time: 1:15 PM
+ */
+
+class Monster
+{
+
+    protected $name;
+
+    function __construct($name = "unknown")
+    {
+        $this-->$name = $name;
+    }
+
+    function getname()
+    {
+        return $this->name;
+    }
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function attack()
+    {
+        echo "<p>" . $this->name . " is attacking <br>";
+    }
+}
